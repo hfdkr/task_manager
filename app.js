6f9e0aa558329f530b7e41d561547e4166a2d42d
hafid <hafidkarkouch5@gmail.com>
2026-05-14 14:53:12 +0100

    feat : modifcation in app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-// require("dotenv").config();
+require("dotenv").config();
 
 const taskRoutes = require("./routes/tasks");
 
